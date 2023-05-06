c5ae06997fcf70a529c9ed3d97176dee5e0523cb
lsunhash24 <lsanhueza2401@gmail.comm>
2023-05-06 14:09:10 -0400

    w
    Q
    quit
    cls
    
    .git/COMMIT_EDITMSG[+] [unix].git/COMMIT_EDITMSG[+].git/COMMIT_EDITMSG[+]

--- Desafio Cachipun/js/cachipun.js
+++ Desafio Cachipun/js/cachipun.js
@@ -1,6 +1,49 @@
+/*
+Crear un programa que le permita a una persona jugar a Piedra, papel o tijera contra el computador,
+indicando cuántas veces desea jugar o repetir el juego.  Piedra, papel o tijera (cachipún) es un juego entre dos
+personas (en este caso, tú y el computador) donde cada una de ellas de manera
+independiente y secreta debe elegir una de las opciones ( piedra, papel y tijera ) y compararla
+con la opción de la otra persona. Para determinar quien gana, se deben seguir las siguientes
+reglas:
+
+● Tijera le gana a papel
+● Papel le gana a piedra
+● Piedra le gana a tijera
+● Si ambos jugadores eligen la misma opción es un empate
+
+1.Solicitar al usuario las veces que desea que se repita el juego consecutivamente, es
+decir, cuántas veces deberá jugar contra la computadora. Por cada juego, se debe
+mostrar el resultado inmediatamente y luego pedir nuevamente una respuesta
+dependiendo de las veces que haya indicado el usuario que desea jugar.
+
+2. Solicitar al usuario que indique su jugada.
+Las opciones son las siguientes:
+    ● Piedra
+    ● Papel
+    ● Tijera
+
+3. Generar una jugada automática para la máquina usando la función Math.random() de Javascript.
+
+4. Definir a un ganador considerando la jugada del usuario y la generada automáticamente para la máquina.
+
+5. Indicar el resultado de la partida dependiendo del caso:
+    ● Felicitar al ganador en caso de ser el usuario.
+    ● Indicarle al usuario que ha perdido contra la máquina en caso de que ésta sea la que gane.
+    ● Declarar un empate.
+
+En Javascript, se pueden crear números aleatorios usando la función Math.random(). Con
+el siguiente código, se pueden obtener números aleatorios entre 0 y 2.
+
+Math.floor(Math.random()*3)
+
+Usando esta función podemos determinar la jugada de la máquina, dado que los resultados de esta
+función pueden ser 0, 1 ó 2, puedes asumir que cada uno de estos números corresponde a
+una jugada ( piedra, papel o tijeras ) y así definir la jugada de la máquina.
+*/
+
 // funcion que determina la juagada de la maquina 
-// obtenida por la función matemátiva math.random()
-function alazar() {
+// obtenida por la función matemática math.random()
+const alazar = () => {
 
     let num = (Math.floor(Math.random() * 3));
     if (num == 0) {
@@ -20,11 +63,11 @@ var acumuladorMaquina = 0;
 var numveces = parseInt(prompt("Ingrese la cantidad de veces a jugar: "));
 
 // se utiliza un ciclo for para generar los juegos según lo indicado por el usuario
-// se utilizan controles de flujos anidados para comparar la elcciones del usuario
+// se utilizan controles de flujos anidados para comparar la elecciones del usuario
 // y la respuesta generada por procesador matemático
 for (let i = 1; i <= numveces; i++) {
     alert(`Este es el juego numero ${i}`);
-    var opcionUsuario = parseInt(prompt("Ingrese opcion cachipun: \n 1.- Tijera \n 2.- Papel \n 3.-Piedra"));
+    let opcionUsuario = parseInt(prompt("Ingrese opcion cachipun: \n 1.- Tijera \n 2.- Papel \n 3.-Piedra"));
     if (opcionUsuario == 1) {
         let opcionMaquina = alazar();
         alert(`Usuario eligió Tijera \n La máquina eligió ${opcionMaquina}`)
